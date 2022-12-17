4121c815a8820f1999e2049441dac8660ede2d8e
abethel912 <abethel180@gmail.com>
2022-12-17 14:38:03 -0500

    Added renderarray and data.js

--- src/components/Proptrick.js
+++
@@ -1,5 +0,0 @@
-function PropTricks(props) {
-  return <h1>PropTricks</h1>
-}
-
-export default PropTricks
---
+++ src/components/RenderArray.js
@@ -0,0 +1,5 @@
+function RenderArray(props) {
+  return <h1>RenderArray</h1>
+}
+
+export default RenderArray;
